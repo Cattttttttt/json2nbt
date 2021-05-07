e3801b53e0790cfe1144e6002cb09976a57cd84b
猫猫 <qwq@dustlover.icu>
2021-05-08 01:03:29 +0800

    Create constant.js

---
+++ src/constant.js
@@ -0,0 +1,11 @@
+export const NBTINTMAX = 2147483647
+export const NBIINTMIN = -2147483648
+export const NBTSHORTMAX = 32767
+export const NBTSHORTMIN = -32768
+export const NBTBYTEMAX = 127
+export const NBTBYTEMIN = -128
+export const NBTLONGMAX = BigInt('9223372036854775807')
+export const NBTLONGMIN = BigInt('-9223372036854775808')
+export const NBTFLOATMAXBIT = 7
+export const NBTDOUBLEMAXBIT = 15
+export const NBTSTRINGMAXLENGTH = 32767
